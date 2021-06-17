e98918dfc824bd6cd6ce9a5fbcfa21097ba27d08
DMD <daodu.muyiwa@gmail.com>
2021-06-17 06:39:35 -0600

    final edit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
-const token = "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";
+const token = sessionStorage.getItem("token");
 const usernameInp = sessionStorage.getItem("username")
-
-
   const query = `
     query { 
       user(login:"${usernameInp}"){
@@ -17,6 +15,7 @@ const usernameInp = sessionStorage.getItem("username")
             updatedAt
             description
             forkCount
+            stargazerCount
             primaryLanguage {
               name
             }
@@ -57,11 +56,16 @@ const renderUI=(data)=>{
   let profileName = document.querySelector('.profile-name')
   profileName.textContent = data.name
 
+  let phoneUserName = document.querySelector(".phone-username")
+  phoneUserName.textContent = `${usernameInp}`;
+
   let profileLink = document.querySelector('.profileLink')
   profileLink.href = data.url
 
-  let smallPics = document.querySelector('.small-pics')
-  smallPics.src = data.avatarUrl
+  let smallPics = document.querySelectorAll('.small-pics')
+  smallPics.forEach(smallPic=>{
+    smallPic.src = data.avatarUrl
+  })
 
   const repoCount = document.querySelector('.repo-count')
   repoCount.textContent = `${data.repositories.totalCount} results for public repositories`
@@ -77,41 +81,129 @@ const renderUI=(data)=>{
       repository.primaryLanguage ={...checker}
     }    
 
+    const bigRepoCont = document.createElement("DIV")
+    bigRepoCont.classList.add("bigRepoCont")
+    
+
     const repositoryCont = document.createElement('DIV')
     repositoryCont.classList.add("repositoryCont")
 
     const repositoryName = document.createElement('P')
+    repositoryName.classList.add("repo-name")
     repositoryName.textContent = repository.name
     const repDescription = document.createElement('P')
+    repDescription.classList.add("repo-description")
     repDescription.textContent = repository.description
 
-    const detailsCont = document.createElement('DIV')
+    const starButtonCont = document.createElement("DIV")
+    const starButton = document.createElement("BUTTON")
+    starButton.innerHTML = `<span><i class="far fa-star transparent"></i><span> Star`
+    starButtonCont.classList.add('star-button-cont')
+    starButton.style.padding = "2px 3px"
+    starButtonCont.appendChild(starButton)
 
+    const detailsCont = document.createElement('DIV')
+    detailsCont.classList.add("details")
+    
+    let primaryLanguageCont = document.createElement('DIV')
+    primaryLanguageCont.classList.add("details-cont")
+    let primarySpan = document.createElement("SPAN")
+    let primaryClasses = ["fa", "fa-circle", "small-details"]
+    let primaryI = document.createElement("I")
+    primaryI.classList.add(...primaryClasses)
+    if (repository.primaryLanguage.name === "JavaScript") {
+      primaryI.classList.add("red")
+    }
+    else if(repository.primaryLanguage.name === "HTML") {
+      primaryI.classList.add("yellow")
+    }
+    else {
+      primaryI.classList.add("blue")
+    }
+    primarySpan.appendChild(primaryI)
     let primaryLanguage = document.createElement("P")
     primaryLanguage.textContent = repository.primaryLanguage.name
+    if(repository.primaryLanguage.name == "") {
+      primarySpan.style.display = "none"
+    }
+    else {
+      primaryLanguageCont.appendChild(primarySpan)
+    }
+    primaryLanguageCont.appendChild(primaryLanguage)
 
+
+    let forkCountCont = document.createElement("DIV")
+    forkCountCont.classList.add("details-cont")
+    let forkSpan = document.createElement("SPAN")
+    let forkSpanClasses = ["fa", "fa-code-branch"]
+    let forki = document.createElement("I")
+    forki.classList.add(...forkSpanClasses)
+    forkSpan.appendChild(forki)
     let forkCount = document.createElement('P')
     forkCount.textContent = repository.forkCount
+    forkCountCont.appendChild(forkSpan)
+    forkCountCont.appendChild(forkCount)
 
+    let stargazerCont = document.createElement("DIV")
+    stargazerCont.classList.add("details-cont")
+    let stargazerSpan = document.createElement("SPAN")
+    let starClasses = ["far", "fa-star", "transparent"]
+    let starI = document.createElement("I")
+    starI.classList.add(...starClasses)
+    stargazerSpan.appendChild(starI)
+    let stargazer= document.createElement("P")
+    stargazer.textContent = repository.stargazerCount
+    stargazerCont.appendChild(stargazerSpan)
+    stargazerCont.appendChild(stargazer)
+
+
+    let updatedAtCont = document.createElement("DIV")
+    updatedAtCont.classList.add("details-cont", "special-details-cont")
     let updatedAt = document.createElement('P')
-    updatedAt.textContent = repository.updatedAt
+    let currentDate = new Date(repository.updatedAt)
+    let changedDate = currentDate.toDateString();
+    const finalEdit = changedDate.slice(4, 15)
+    updatedAt.textContent = `Updated on ${finalEdit}`;
+    updatedAtCont.appendChild(updatedAt)
 
-  
     
-    detailsCont.appendChild(primaryLanguage)
-    detailsCont.appendChild(updatedAt)
-    detailsCont.appendChild(forkCount)
+    
+    detailsCont.appendChild(primaryLanguageCont)
+    detailsCont.appendChild(stargazerCont)
+    detailsCont.appendChild(forkCountCont)
+    detailsCont.appendChild(updatedAtCont)
+    
+
+
     repositoryCont.appendChild(repositoryName)
     repositoryCont.appendChild(repDescription)
     repositoryCont.appendChild(detailsCont)
-    repoContainer.appendChild(repositoryCont)
-    repositories.appendChild(repositoryCont)
+
+    bigRepoCont.appendChild(repositoryCont)
+    bigRepoCont.appendChild(starButtonCont)
+
+    repoContainer.appendChild(bigRepoCont)
+    repositories.appendChild(repoContainer)
   })
 }
 
 fetchData()
 
+const navbar = document.querySelector(".nav-bar")
+const burger = document.querySelector(".burger")
+burger.addEventListener("click", ()=>{
+  navbar.classList.toggle("nav-active")
+  burger.classList.toggle("toggle")
+})
 
+const search = document.querySelector(".search")
+const searchCont = document.querySelector(".search-cont")
 
-
-
+search.addEventListener("click", ()=>{
+  if(search === document.activeElement) {
+    searchCont.classList.add("width")
+  }
+  else {
+    searchCont.classList.remove("width")
+  }
+})
--- script1.js
+++ script1.js
@@ -2,13 +2,26 @@ const submit = document.querySelector('.submit')
 const usernameInp = document.querySelector('#username')
 const issues = document.querySelector('.issues')
 
+const token  = document.querySelector("#token")
+
 
 submit.addEventListener('click', (e)=>{
   if(usernameInp.value === "") {
     e.preventDefault()
     issues.textContent = "username must be entered"
+    setTimeout(()=>{
+      issues.textContent = ""
+    },3000)
+  }
+  else if (token.value === "") {
+    e.preventDefault()
+    issues.textContent = "please enter a valid token"
+    setTimeout(()=>{
+      issues.textContent = ""
+    }, 3000)
   }
   else {
     sessionStorage.setItem('username', usernameInp.value)
+    sessionStorage.setItem('token', token.value)
   }
 })
